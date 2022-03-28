e5b740e309e502adadb23ed18313c2806349aab5
0IIIIM0 <ignac23med@gmail.com>
2022-03-28 11:53:17 -0600

    Initial commit

---
+++ src/Ejercicio1.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class Compare_the_triplex {
+    public static void main(String[] args) {
+        public static List<Integer> compareTriplets(List<Integer>)
+    }
+}
