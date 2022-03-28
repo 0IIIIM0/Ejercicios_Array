9481accdbb01f8910bfe6abd4d67d34369e0eb31
0IIIIM0 <ignac23med@gmail.com>
2022-03-28 11:56:33 -0600

    COMIT INITIAN

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -2,6 +2,14 @@ import java.util.List;
 
 public class Compare_the_triplex {
     public static void main(String[] args) {
-        public static List<Integer> compareTriplets(List<Integer>)
+        public static List<Integer> compareTriplets(List<Integer>a,List<Integer>b){
+            int contA=0,contB=0;
+            for (int i = 0; i < a.size; i++) {
+                if (a.get(i)>b.get(i)){
+                    contA++;
+
+                }
+            }
+        }
     }
 }
